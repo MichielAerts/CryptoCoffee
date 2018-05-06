b201c766edcd2ef44c8aa9d18e8aa57620e59660
Michiel Aerts <michiel.aerts@gmail.com>
2018-05-06 17:54:27 +0200

    dummy script 4

--- scripts/rfid.sh
+++ scripts/rfid.sh
@@ -1,3 +1,23 @@
-#!/bin/sh
-# This is a comment!
-echo Hello World	# This is a comment, too!
+#!/bin/bash
+#            // script should:
+#            // 1. accept one argument containing the uuid
+#            // 2. retrieve the rfid
+#            // 3. create a file in the rfid_map directory named as the uuid,
+#            //    containing a line with format <uuid>:<retreived rfid>
+#            // 4. return exit status 0 if all is ok.
+
+set -x
+
+if [ $# -ne 1 ]
+then
+  echo "We expect one input argument, the uuid for the call"
+  return 1
+fi
+
+uuid=$1
+rfid_storage_map=/home/pi/Documents/Java/CryptoCoffee/rfids/
+fake_rfid=$RANDOM
+
+sleep 1
+
+echo ${uuid}:${fake_rfid} >> ${rfid_storage_map}${uuid}
